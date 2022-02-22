c69d6e7df36e3464ae4562f222c743003c490ff9
Trinity <elizabethtrinitygs@gmail.com>
2022-02-21 21:11:06 -0600

    configuracion idioma y timezone

--- trinityminiblog/settings.py
+++ trinityminiblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^$_f8j%^jz4#5f#*)zpl5t@oxb=)i2y@p!qpmqgh$-s#6&nfev
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Mexico/General'
 
 USE_I18N = True
 
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
